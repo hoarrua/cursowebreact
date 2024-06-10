a414e73534cc6394c40a5b43759a412da53ba119
Arrua Hugo <hoarrua@gmail.com>
2024-06-10 17:53:27 -0300

    se sube poryecto react

---
+++ Modulo 4/Unidad 1/web-react/src/pages/ContactoPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/normalize.css';
+import '../styles/transportes.css';
+
+const Contacto = (props) => {
+	return (
+		<div>
+			<p>CONTACTO</p>
+		</div>
+	);
+}
+
+export default Contacto;
